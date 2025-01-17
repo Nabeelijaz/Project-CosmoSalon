cdba7529ea20f7ebf4f4434dca2bcb1388de26ba
Nabeel_Ijaznabinoor <nnabeelijaznabinoor@gmail.com>
2025-01-17 18:52:57 +0500

    Popup Html and css

--- CosmoSalon/assets/js/popup.js
+++ CosmoSalon/assets/js/popup.js
@@ -2,14 +2,14 @@
 const appointmentPopup = document.getElementById("appointmentPopup");
 const appointmentPopupOpen = document.getElementById("appointmentPopupOpen");
 const appointmentPopupClose = document.getElementById("appointmentPopupClose");
-console.log(appointmentPopup);
 
-appointmentPopup.addEventListener("click", function () {
-  appointmentPopupOpen.style.display = "block";
+appointmentPopupOpen.addEventListener("click", function () {
+  appointmentPopup.style.display = "block";
+  // appointmentPopup.innerHTML = `test`;
 });
 
 appointmentPopupClose.addEventListener("click", function () {
-  if ((appointmentPopupOpen.style.display = "block")) {
-    appointmentPopupOpen.style.display = "none";
+  if (appointmentPopup.style.display === "block") {
+    appointmentPopup.style.display = "none";
   }
 });
