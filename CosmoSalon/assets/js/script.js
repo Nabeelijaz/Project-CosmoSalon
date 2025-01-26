c50b1da1598839bbdd2200c12606d395d6489f49
Nabeel_Ijaznabinoor <nnabeelijaznabinoor@gmail.com>
2025-01-26 17:10:43 +0500

    Add form validation and error messages for appointment booking

--- CosmoSalon/assets/js/script.js
+++ CosmoSalon/assets/js/script.js
@@ -1,5 +1,5 @@
 const images = [
-  "file:///C:/Users/MMT/Desktop/Project%20Web%20Technologies/CosmoSalon/assets/images/image.png",
+  "../images/image.png",
   "../images/image1.png",
   "../images/image2.png",
   "../images/image3.png",
@@ -26,3 +26,42 @@ rightArrow.addEventListener("click", () => {
 });
 
 changeBackground(currentIndex);
+
+// ---------- FormData ----------
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let isValid = true;
+
+  const location = document.getElementById("location").value;
+  const gender = document.getElementById("gender").value;
+  const service = document.getElementById("service").value;
+  const availableDate = document.getElementById("available-date").value;
+  const startTime = document.getElementById("start-time").value;
+  const endTime = document.getElementById("end-time").value;
+
+  if (location == "0") {
+    isValid = false;
+    alert("Please select a location.");
+  }
+  if (gender == "0") {
+    isValid = false;
+    alert("Please select a gender.");
+  }
+  if (service == "0") {
+    isValid = false;
+    alert("Please select a service.");
+  }
+  if (availableDate == "") {
+    isValid = false;
+    alert("Please select an available date.");
+  }
+  if (startTime == "" || endTime == "") {
+    isValid = false;
+    alert("Please select valid start and end times.");
+  }
+
+  if (isValid) {
+    alert("Form is valid. Proceeding to the next step.");
+  }
+});
