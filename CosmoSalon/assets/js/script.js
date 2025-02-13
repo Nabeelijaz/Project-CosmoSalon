5aa0df19b2421c479fc0178f765bbc6edd9c2092
Nabeel_Ijaznabinoor <nnabeelijaznabinoor@gmail.com>
2025-02-13 22:31:02 +0500

    Added Bootstrap icons for phone, location, and time in sidebar

--- CosmoSalon/assets/js/script.js
+++ CosmoSalon/assets/js/script.js
@@ -84,3 +84,10 @@ setInterval(() => {
   currentIndex = (currentIndex + 1) % images.length;
   imgSlider.style.backgroundImage = `url(${images[currentIndex]})`;
 }, 10000);
+
+document.getElementById("openSidebar").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.add("active");
+});
+document.getElementById("closeSidebar").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.remove("active");
+});
